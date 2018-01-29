28dabe01f47dfe091ea922b3ef7876cc4d421380
jared frankel <jaredfrankel10@gmail.com>
2018-01-29 21:24:58 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,11 +2,11 @@ require 'artist'
  
  class Song
    attr_accessor :name, :artist
- 
+
    def initialize(name)
      @name = name
    end
- 
+
    def self.new_by_filename(filename)
      artist_name = filename.split(" - ")[0]
      song_name = filename.split(" - ")[1]
@@ -16,5 +16,5 @@ require 'artist'
      artist.save
      song
    end
- 
+
  end
